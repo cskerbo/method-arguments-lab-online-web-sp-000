f1651f0654b8a6ddc42ef5a24379ad4f7769c9bb
Chris Kerbo <chris.kerbo@outlook.com>
2019-09-13 23:28:15 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,5 @@
-def
+def introdutction(name)
+  puts "Hi, my name is #{name}."
 
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
